c0370bb2109a33a11c3b215006dec4f0de4fddc5
Tayphoon <evgeny.kulinchenko@gmail.com>
2012-04-06 10:02:10 +0400

    Fix SDWebImage

--- build/SDWebImage.build/Release-iphoneos/SDWebImage.build/Objects-normal/armv7/SDWebImageDecoder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageDecoder.m \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageDecoder.h \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageCompat.h
