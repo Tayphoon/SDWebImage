fcad9f5ec4169246ed30ed462402969f91e040bb
Tayphoon <evgeny.kulinchenko@gmail.com>
2012-04-12 15:40:50 +0400

    Change

--- build/SDWebImage.build/Debug-iphonesimulator/SDWebImage.build/Objects-normal/i386/UIImageView+WebCache.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/UIImageView+WebCache.m \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/UIImageView+WebCache.h \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageCompat.h \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageManagerDelegate.h \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageManager.h \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDWebImageDownloaderDelegate.h \
-  /Users/Tayphoon/Projects/SideCastr/ipad-client/Externals/SDWebImage/SDImageCacheDelegate.h
